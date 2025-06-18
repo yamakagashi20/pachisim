c42fe86008177e9d1426e92acef5979539d01a1d
yamakagashi20 <dkkozaki512@gmail.com>
2025-06-18 09:50:39 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PachinkoGame from './components/PachinkoGame';
+
+function App() {
+  return (
+    <div>
+      <PachinkoGame />
+    </div>
+  );
+}
+
+export default App;
